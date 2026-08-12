6bcb10e4c4e21bdcc523e0c1c1355595724cc347
phamtuan2005tm-cpu <email_cua_fen@example.com>
2026-08-12 22:51:04 +0700

    update RegisterRequest : Remove

--- GreenCart/GreenCart/Dtos/Requests/Auth/RegisterRequest.cs
+++ GreenCart/GreenCart/Dtos/Requests/Auth/RegisterRequest.cs
@@ -4,11 +4,7 @@ namespace GreenCart.Dtos.Requests.Auth
 {
     public class RegisterRequest
     {
-        [Required]
         public string FullName { get; set; } = string.Empty;
-
-        [Required]
-        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
         

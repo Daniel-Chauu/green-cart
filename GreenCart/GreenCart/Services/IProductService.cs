6d02d1eed746e323ad1097a503fafadf6931d50a
tu.dong.cit23 <tu.dong.cit23@eiu.edu.vn>
2026-08-12 14:53:26 +0700

    Add repository, service and product API

---
+++ GreenCart/GreenCart/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using GreenCart.Dtos.Requests.Products;
+using GreenCart.Dtos.Responses.Products;
+using GreenCart.Repositories.Helpers;
+
+namespace GreenCart.Services
+{
+    public interface IProductService
+    {
+        Task<ProductResponse> CreateProductAsync(CreateProductRequest request);
+        Task<ProductResponse> UpdateProductAsync(int id, UpdateProductRequest request);
+        Task<bool> DeleteProductAsync(int id);
+        Task<ProductResponse?> GetProductByIdAsync(int id);
+        Task<PagedResult<ProductResponse>> GetProductsAsync(ProductFilterParams filterParams);
+    }
+}
